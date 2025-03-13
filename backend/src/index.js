14040343ff99928cd73232539402d829088ae397
SCRIFT1\sikan <munirabbasi2001@gmail.com>
2025-03-14 04:04:17 +0500

    prod fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -57,7 +57,7 @@ const PORT = process.env.PORT;
 // }));
 // // Middleware
 // app.use(express.json());
-
+app.use(express.json());  // 👈 This should be BEFORE defining your routes!
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
 // export const API_URL = "http://10.0.2.2:5001";
-export const API_URL = "http://192.168.1.101:5001";
-// 
+// export const API_URL = "http://192.168.1.101:5001";
+export const API_URL="https://sharetrack-backend.onrender.com";
